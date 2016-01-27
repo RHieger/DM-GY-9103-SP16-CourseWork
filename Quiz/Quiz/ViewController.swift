2927fe40a3fee8e20a10ceb66d0715c981c5d196
Bobby <rhieger@nyc.rr.com>
2016-01-27 17:41:35 -0500

    Chapter 1 Quiz--Moved Incorrectly placed array declarations for questions and answers

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -16,19 +16,21 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] =
+    ["From what is cognac made?",
+        "What is 7 + 7?",
+        "What is the capital of Vermont?"]
+    
+    let answers: [String] =
+    ["Grapes",
+        "14",
+        "Montpelier"]
+    
+    var currentQuestionIndex: Int = 0
+    
     @IBAction func showNextQuestion(sender: AnyObject)  {
         
-        let questions: [String] =
-        ["From what is cognac made?",
-         "What is 7 + 7?",
-         "What is the capital of Vermont?"]
         
-        let answers: [String] =
-        ["Grapes",
-         "14",
-         "Montpelier"]
-        
-        var currentQuestionIndex: Int = 0
         
     }   // end showNextQuestion(sender: AnyObject)
     
