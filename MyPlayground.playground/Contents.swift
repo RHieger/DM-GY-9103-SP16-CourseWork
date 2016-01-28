a743930bf4bec428f9c3a1d5c65896c0568c281f
Bobby <rhieger@nyc.rr.com>
2016-01-28 14:12:43 -0500

    Chapter 2 MyPlayground.playground--Work with Strings and Constants: pages 91-94

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+//: First playground for Chapter 2
+
+import Cocoa
+
+var str = "Hello, playground"
+
+str = "Hello, Swift"
+
+// Set new constant to the value of str.
+
+let constString = str
+
