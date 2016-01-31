15807edb1168125ebf5c59e9968ec442eb24454f
Bobby <rhieger@nyc.rr.com>
2016-01-31 11:02:23 -0500

    Chapter 2 MyPlayground.playground--Declare Collection Types Array, Dictionary & Set: pages 97-99

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -22,3 +22,32 @@ var nextYear: Int
 var bodyTemp: Float
 
 var hasPet: Bool
+
+/* The next few lines will explore the use of collections, of which there
+ * are three types in Swift: Array, Set and Dictionary. There are two methods
+ * of declaration, one verbose and the shorthand syntax. Both are explored
+ * below for clarity.
+*/
+
+// Explicit declaration:
+
+//var arrayOfInts: Array<Int>
+
+// Shorthand declaration:
+
+var arrayOfInts: [Int]
+
+// Explicit declaration of dictionary:
+
+//var dictionaryOfCapitalsByCountry: Dictionary<String, String>
+
+// Shorthand declaration of dictionary:
+
+var dictionaryOfCapitalsByCountry: [String: String]
+
+// Declare a collection variable of type Set, which appears in the book
+// to have only an explicit declaration syntax:
+
+var winningLotteryNumbers: Set<Int>     // NOTE: Only elements of Type Int
+                                        // may be part of this Set.
+
