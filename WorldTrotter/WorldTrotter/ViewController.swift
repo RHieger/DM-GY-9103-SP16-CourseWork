fec80eb11253cd52bb216837249bf3b291bb7036
Bobby <rhieger@nyc.rr.com>
2016-02-02 21:10:11 -0500

    Chapter 3 WorldTrotter--Use Interface Builder to Build Dummy UI: pages 138-141

--- WorldTrotter/WorldTrotter/ViewController.swift
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -10,48 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        
-        // Call the superclass we wish to override.
-        
-        super.viewDidLoad()
-        
-        
-        // Build first frame view from a CGRect.
-        
-        let firstFrame =
-        CGRect(x: 160, y: 240, width: 100, height: 150)
-        
-        // Create superview to contain CGRect.
-        
-        let firstView = UIView(frame: firstFrame)
-        
-        // Set color for firstFrame.
-        
-        firstView.backgroundColor = UIColor.blueColor()
-        
-        // Add subview to UIView.
-        
-        view.addSubview(firstView)
-        
-        // Build another frame view from a CGRect.
-        
-        let secondFrame =
-        CGRect(x: 20, y: 30, width: 50, height: 50)
-        
-        // Create superview to contatin CGRect
-        
-        let secondView = UIView(frame: secondFrame)
-        
-        // Set color for secondFrame CGRect.
-        
-        secondView.backgroundColor = UIColor.greenColor()
-        
-        // Add secondView to firstView subview.
-        
-        firstView.addSubview(secondView)
-        
-    }   // end viewDidLoad()
+    
 
 }   // end ViewController: UIViewController
 
