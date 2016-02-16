20779fc6726d4150bf0ac74d69d5a54bf3bc68ed
Bobby <rhieger@nyc.rr.com>
2016-02-16 10:49:05 -0500

    Chapter 6 WorldTrotter--Created MKMapView Programatically: pages 229-232

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -8,7 +8,31 @@
 
 import UIKit
 
+import MapKit       // Required to create an MKMapView
+
 class MapViewController: UIViewController {
+    
+    // PROPERTIES:
+    
+    var mapView: MKMapView!     // Forcibly unwrapped as this
+                                // reference is required to
+                                // create mapView
+    
+    // METHODS:
+    
+    // Override func loadView().
+    
+    override func loadView() {
+        
+        // Instantiate a map view object.
+        
+        mapView = MKMapView()
+        
+        // Set it as *the* view of this UIViewController object.
+        
+        view = mapView
+        
+    }
     
     // Override func viewDidLoad().
     
@@ -20,6 +44,8 @@ class MapViewController: UIViewController {
         
         print("MapViewController loaded its view.")
         
-    }
+    }   // end override func viewDidLoad()
+    
+    // Override
     
 }   // end MapViewController
