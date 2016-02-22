78d1d8cbb75a233ff79a1008842f32924ac7dcb2
Bobby <rhieger@nyc.rr.com>
2016-02-22 03:14:30 -0500

    Chapter 8 Quiz--Updated UIView.animatedWithDuration to Swap Question Labels: pages 307-308

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -106,14 +106,22 @@ class ViewController: UIViewController {
         
         // Animate the alpha:
         
-        UIView.animateWithDuration(0.5, animations: {
-            
-            self.currentQuestionLabel.alpha = 0
-            
-            self.nextQuestionLabel.alpha = 1
-        
-        } )
-        
+        UIView.animateWithDuration(0.5,
+            delay: 0,
+            options: [],
+            animations: {
+                
+                self.currentQuestionLabel.alpha = 0
+                self.nextQuestionLabel.alpha = 1
+                
+            },
+            completion: { _ in
+                
+              swap(&self.currentQuestionLabel,
+                   &self.nextQuestionLabel)
+                
+            } )
+    
     }   // end animateLabelTransitions()
 
 }   // end viewController
