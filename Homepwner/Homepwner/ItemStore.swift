233cac066d7e422907f142e8541d2b9508876bd4
Bobby <rhieger@nyc.rr.com>
2016-03-03 01:30:48 -0500

    Chapter 9 Homepwner--Added ItemStore Class Skeleton: page 338-340

---
+++ Homepwner/Homepwner/ItemStore.swift
@@ -0,0 +1,23 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Robert Hieger on 3/3/16.
+//  Copyright © 2016 Hieger Designs. All rights reserved.
+//
+
+import UIKit
+
+/********************************************************
+ *                                                      *
+ * The ItemStore class encapsulates the instantiation   *
+ * of an array of Item class instances.                 *
+ *                                                      *
+ ********************************************************
+*/
+
+class ItemStore {
+    
+    // Place code here.
+    
+}   // end ItemStore
