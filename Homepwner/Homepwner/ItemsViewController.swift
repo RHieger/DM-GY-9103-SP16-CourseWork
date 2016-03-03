5e401fe3d20d4bfb09ae1eb11c6378d968a53b0b
Bobby <rhieger@nyc.rr.com>
2016-03-03 00:17:36 -0500

    Chapter 9 Homepwner--Added ItemsViewController Skeleton: page 330

---
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Robert Hieger on 3/3/16.
+//  Copyright © 2016 Hieger Designs. All rights reserved.
+//
+
+import UIKit
+
+/*********************************************************
+ *                                                       *
+ * The ItemsViewController class will control a table    *
+ * view that pulls data from a data store into the       *
+ * UITableViewCell objects that are part of its view.    *
+ *                                                       *
+ *********************************************************
+*/
+
+class ItemsViewController: UITableViewController {
+    
+    // Place code here.
+    
+}   //  end ItemsViewController
