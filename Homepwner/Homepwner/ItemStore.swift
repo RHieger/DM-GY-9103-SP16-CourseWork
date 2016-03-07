b8cd867cf7080d94d6de724091f8bfa56126d6bc
Bobby <rhieger@nyc.rr.com>
2016-03-06 21:30:50 -0500

    Chapter 10 Homepwner--Removed Unneeded Designated Initializer from ItemStore Class: page 374

--- Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/ItemStore.swift
@@ -41,19 +41,4 @@ class ItemStore {
         
     }   // end createItem() -> Item
     
-    
-    // DESIGNATED INITIALIZER:
-    
-    init()  {
-        
-        for _ in 0..<5 {
-            
-            // Instantiate 5 Item objects.
-            
-            createItem()
-            
-        }   // end for _ in
-        
-    }
-    
 }   // end ItemStore
