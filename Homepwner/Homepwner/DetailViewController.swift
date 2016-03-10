1aa348f44222abd4e38c12345247358b311ba7fa
Bobby <rhieger@nyc.rr.com>
2016-03-09 19:33:18 -0500

    Chapter 12 Homepwner--Added DetailViewController Skeleton: page 435

---
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Robert Hieger on 3/9/16.
+//  Copyright © 2016 Hieger Designs. All rights reserved.
+//
+
+import UIKit
+
+/**********************************************************
+ *                                                        *
+ * DetailViewController Class:                            *
+ *                                                        *
+ * This class pulls data from a tapped tableViewCell      *
+ * and displays them in in the Detail Stack View. This    *
+ * controller also controls the transmission of this      *
+ * data to the nested stack views.                        *
+ *                                                        *
+ **********************************************************
+*/
+
+class DetailViewController: UIViewController {
+    
+    // Place code here.
+    
+}   // end DetailViewController
