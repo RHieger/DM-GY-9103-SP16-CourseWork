af480ec49f30b14492065aa24af846e533cb3cd7
Bobby <rhieger@nyc.rr.com>
2016-03-21 19:41:13 -0400

    Chapter 14 Homepwner--Created ImageStore Class Stub: page 506

---
+++ Homepwner/Homepwner/ImageStore.swift
@@ -0,0 +1,31 @@
+//
+//  ImageStore.swift
+//  Homepwner
+//
+//  Created by Robert Hieger on 3/21/16.
+//  Copyright © 2016 Hieger Designs. All rights reserved.
+//
+
+import UIKit
+
+/**********************************************************
+ *                                                        *
+ * ImageStore() Class                                     *
+ *                                                        *
+ * The ImageStore() class stores images taken by the      *
+ * user of Homepwner, allows those to be read into        *
+ * memory for display on the UIImageView, and provides    *
+ * a cache that gives quick access to these images, but   *
+ * flushes the memory of the references to these images   *
+ * when a low memory warning is received. This prevents   *
+ * the application from crashing due to running out of    *
+ * available memory.                                      *
+ *                                                        *
+ **********************************************************
+*/
+
+class ImageStore {
+    
+    // Place code here.
+    
+}
