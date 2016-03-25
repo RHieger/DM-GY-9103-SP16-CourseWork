3d3fe27c285f8837cc95f5f4483792f82168a52d
Bobby <rhieger@nyc.rr.com>
2016-03-25 14:15:14 -0400

    Chapter 15 Homepwner--Implemented NSKeyedArchiver to Save Items for Persistence: pages 538-541

--- Homepwner/Homepwner/AppDelegate.swift
+++ Homepwner/Homepwner/AppDelegate.swift
@@ -12,19 +12,18 @@ import UIKit
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    // MARK: - Main Interface Window
+    // MARK: - Properties
     
     var window: UIWindow?
+    
+    let itemStore = ItemStore()   // Provides reference for
+                                  // saveChanges() method
 
-    // MARK: - Application Entry Point
+    // MARK: - Methods
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         // Override point for customization after application launch.
-        
-        // Instantiate ItemStore object at application launch:
-        
-        let itemStore = ItemStore()
         
         // Instantiate ImageStore object at application launch:
         
@@ -56,9 +55,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
+        
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+        
+        // When user touches Home buttton on phone, call
+        // ItemStore.saveChanges().
+        
+        let success = itemStore.saveChanges()
+        
+        if (success)    {
+            
+            // Print debug information to console.
+            
+            print("Saved all of the Items.")
+            
+        }   else    {
+            
+            // Print debug information to console.
+            
+            print("Could not save any of the Items.")
+            
+        }   // end if-else
+        
+    }   // end applicationDidEnterBackground(application: UIApplication)
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
