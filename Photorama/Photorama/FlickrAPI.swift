a962acda39e63f068f8c81e333a74df2f81671ca
Bobby <rhieger@nyc.rr.com>
2016-04-17 13:51:12 -0400

    Chapter 19: Photorama--Created FlickerAPI Struct Skeleton: pages 668-670

---
+++ Photorama/Photorama/FlickrAPI.swift
@@ -0,0 +1,27 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by Robert Hieger on 4/17/16.
+//  Copyright © 2016 Hieger Designs. All rights reserved.
+//
+
+/**************************************************************
+ *                                                            *
+ * FlickrAPI Struct:                                          *
+ *                                                            *
+ * The FlickrAPI type, implemented as a struct, defines the   *
+ * structure of a request for the Flickr API, and, together   *
+ * with the PhotoStore class, sends URL requests to Flicker   *
+ * to retrieve photos.                                        *
+ *                                                            *
+ **************************************************************
+*/
+
+import Foundation
+
+struct FlickerAPI {
+    
+    // Insert fields here.
+    
+}   // end flickerAPI
