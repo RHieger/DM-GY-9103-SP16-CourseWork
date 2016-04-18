80e723bd1498c80d011899d36c4169a88b04c934
Bobby <rhieger@nyc.rr.com>
2016-04-18 02:11:27 -0400

    Chapter 19 Photorama--Created Photo Class Skeleton: page 685

---
+++ Photorama/Photorama/Photo.swift
@@ -0,0 +1,26 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Robert Hieger on 4/18/16.
+//  Copyright © 2016 Hieger Designs. All rights reserved.
+//
+
+/**************************************************************
+ *                                                            *
+ * Photo Class:                                               *
+ *                                                            *
+ * This class defines the structure and initializes photo     *
+ * objects specified in the JSON data returned from the       *
+ * PhotoStore class.                                          *
+ *                                                            *
+ **************************************************************
+*/
+
+import Foundation
+
+class Photo {
+    
+    
+    
+}   // end Photo
