8c0998e07193c3aa7b3d9758d8e83f4e76ba5dae
Bobby <rhieger@nyc.rr.com>
2016-04-18 00:54:21 -0400

    Chapter 19 Photorama--Created Photostore Class Skeleton: pages 678-679

---
+++ Photorama/Photorama/PhotoStore.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoStore.swift
+//  Photorama
+//
+//  Created by Robert Hieger on 4/18/16.
+//  Copyright © 2016 Hieger Designs. All rights reserved.
+//
+
+/************************************************************
+ *                                                          *
+ * PhotoStore Class:                                        *
+ *                                                          *
+ * This class is responsible for sending the request to     *
+ * the Flickr server and interpreting the server's          *
+ * response to the query.                                   *
+ *                                                          *
+ ************************************************************
+*/
+
+import Foundation
+
+class PhotoStore    {
+    
+    
+    
+    
+}   // end PhotoStore
